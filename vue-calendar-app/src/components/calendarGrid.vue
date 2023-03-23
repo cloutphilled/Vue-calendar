7e757c11da9962068e8d6648a8326cf89e032bcf
Phillip Friis Petersen <74956595+cloutphilled@users.noreply.github.com>
2023-03-23 14:52:27 +0000

    first calendar version

---
+++ vue-calendar-app/src/components/calendarGrid.vue
@@ -0,0 +1,69 @@
+<script setup>
+    const DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+</script>
+<template>
+    <div class="calndar-grid">
+        <div class="days-grid">
+            <div
+                v-for="day, index in DAYS"
+                :key="index"
+                class="day"
+                :title="day"
+            >{{ day[0] }}</div>
+        </div>
+        <div class="date-grid">
+            <div class="date"
+                v-for="date in range(31)"
+            >
+                <div
+                    class="val"
+                >{{ date }}</div>
+        </div>
+        </div>
+    </div>
+</template>
+<style scoped lang="less">
+    .calendarGrid{
+        margin-top: 15px;
+        display: grid;
+        gap: 20px;
+        user-select: none;
+        .days-grid{
+            display: grid;
+            grid-template-columns: repeat(7,1fr);
+            justify-content: space-between;
+            align-items: center;
+            .day{
+                text-align: center;
+                font-weight: bold;
+                &.today{
+                    color:blueviolet;
+                }
+            }
+        }
+        .date-grid{
+            display: grid;
+            grid-template-columns: repeat(7,1fr);
+            justify-content: space-between;
+            align-items: center;
+            row-gap: 20px;
+            .date{
+                display: flex;
+                justify-content: center;
+                cursor: pointer;
+                .val{
+                    width: 20px;
+                    height: 20px;
+                    text-align: center;
+                    border-radius: 50%;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 15px;
+                    position: relative;
+                    background-color: antiquewhite;
+                }
+            }
+        }
+    }
+</style>
